13cdf0a1e8c51725e11b87ee027534d06027cc24
Maria sophia Mascardo <mariasophiamascardo@Pias-MacBook-Air.local>
2024-10-23 18:22:14 +0200

    solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,16 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a , b) {
+        if(a > b){
+            return a
+        }
+        else if(a < b) {
+            return b
+        }
+        else {
+            return a = b 
+        }
+    }
+
 
 
 
@@ -7,23 +18,51 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0) return null;
+    let longestWord = words[0];
 
+   for (let i = 0; i < words.length; i++){
+    if (words[i].length > longestWord.length){
+        longestWord= words[i];
+    }
+}    
+   return longestWord;
+}
+ 
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0;
+    for (i =0; i < numbers.length; i++){
+        sum += numbers[i]
+    }
+    return sum
+}
 
+   
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    let sum = 0;
+    for (i = 0; i < numbers2.length; i++){
+        sum += numbers2[i]  
+    }
+    return sum
+}
+function averageNumbers(numbers2){
+    if (numbers2.length === 0) return 0;
+    return sumNumbers(numbers2)/ numbers2.length;
+}
+
 
 
 
@@ -31,4 +70,13 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(wordsArray, searchWord) {
+ if (wordsArray.length === 0) return null;
+
+ for (let i =0; i < wordsArray.length; i++){
+    if (wordsArray[i] === searchWord){
+        return true;
+    }
+ }
+ return false;
+}
